2aad7d5240ace5cf35788a1e0fb8262296d86b02
Bhaavan Merchant <bhaavanmerchant@gmail.com>
2013-05-30 13:54:31 +0530

    Sorted a symbolic link mess

--- facebook_unclutter.js
+++ facebook_unclutter.js
@@ -1,1 +1,40 @@
-/home/bha1/.mozilla/firefox/7wty98nw.default/gm_scripts/Facebook_Cleaner/Facebook_Cleaner.user.js
+// ==UserScript==
+// @name        Facebook Unclutter
+// @namespace   com.userscript.facebook.bhaavanmerchant.cleaner
+// @include     https://www.facebook.com/
+// @include     *.facebook.*
+// @include     facebook.com/*
+// @include     htt*://*.facebook.com/*
+// @grant 		metadata
+// @version     1
+// @exclude     htt*://*channel*.facebook.com*
+// @exclude     htt*://*static*.facebook.com*
+// @exclude     htt*://upload.facebook.com/*
+// @exclude     htt*://*.facebook.com/ajax/*
+// ==/UserScript==
+
+function cleanUp() {
+    
+    var nodes = document.body.getElementsByClassName('clearfix storyContent');
+    if (nodes != null && typeof nodes !== "undefined" ) 
+	{        
+		for (var i = 0; i < nodes.length; i++) {            
+            if (nodes[i].innerHTML.indexOf("href=\"/about/ads\"") != -1)
+            {
+                nodes[i].style.display = 'none';           
+            }
+		}
+	}
+    document.getElementById('pagelet_ego_pane_w').style.display = 'none';
+    document.getElementById('pagelet_ego_pane').style.display = 'none';
+    document.getElementById('pagelet_navigation').style.display = 'none';
+    var nodesU = document.body.getElementsByClassName('storyUnconnectedPost');
+    if (nodesU != null && typeof nodesU !== "undefined" ) 
+	{
+        for (var i = 0; i < nodesU.length; i++) {
+            nodesU[i].style.display = "none";
+        }
+    }
+}
+
+document.addEventListener("DOMNodeInserted", cleanUp, true);
